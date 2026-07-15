72efd610aa01115852d58efbcbbb27b2283fb88c
tduncan14 <nbatre1@yahoo.com>
2026-07-14 20:12:11 -0500

    keep moving forward

---
+++ frontend/src/app/components/Speakers/Speaker.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Card from '../Card/Card'
+import './Speaker.css'
+
+interface Speaker {
+    id: string;
+    name: string;
+    role: string;
+    company: string;
+    avatar: string;
+    bio: string;
+    featured: boolean;
+}
+
+const Speaker = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Speaker
